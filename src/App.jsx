ede02dabd65a9fd42943abc5d2f5c266d435a5f9
Jinesh R <jineshr101@gmail.com>
2026-01-11 21:01:49 +0530

    complete movies page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import Movie from "./pages/Movie";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/movies" element={<Movie />} />
       </Routes>
     </>
   );
